b61395bfb95788a4e528e1fa74e973132fabf976
Chike Nnamadim <38839028+Perfected1@users.noreply.github.com>
2020-07-30 04:50:49 +0100

    add routes files

---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const passport = require('passport');
+const router = express.Router()
+
+
+// @desc Auth with google
+// @route GET /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+
+// @desc Google auth callback
+// @route GET /auth/google/callback
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+        res.redirect('/dashboard')
+    }
+)
+
+// @desc Auth with facebook
+// @route GET /auth/facebook
+router.get('/facebook', passport.authenticate('facebook', { scope: ['profile'] }))
+
+// @desc Facebook auth callback
+// @route GET /auth/facebook/callback
+router.get('/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }),
+    (req, res) => {
+        res.redirect('/dashboard')
+    }
+)
+//@desc Logout User
+//@route /auth/logout
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
+module.exports = router
